6a4cb7ea3ec97c5359d776827f99bb4009ffaac6
Download-Fritz <download-fritz@outlook.com>
2017-06-16 21:20:48 +0200

    Add TSC GUID and UI Theme + Partition Info protocols.

---
+++ Include/Guid/AppleTscFrequency.h
@@ -0,0 +1,29 @@
+/** @file
+  Copyright (C) 2005 - 2017, Apple Inc.  All rights reserved.
+  Portions Copyright (C) 2017, CupertinoNet.  All rights reserved.<BR>
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+**/
+
+#ifndef APPLE_TSC_FREQUENCY_GUID_H_
+#define APPLE_TSC_FREQUENCY_GUID_H_
+
+// APPLE_TSC_FREQUENCY_GUID
+#define APPLE_TSC_FREQUENCY_GUID  \
+  { 0x674ABEA3, 0x0FE5, 0x11E5,   \
+      { 0x98, 0x8E, 0xB8, 0xE8, 0x56, 0x2C, 0xBA, 0xFA } }
+
+// gAppleTscFrequencyGuid
+extern EFI_GUID gAppleTscFrequencyGuid;
+
+#endif // APPLE_TSC_FREQUENCY_GUID_H_
---
+++ Include/Protocol/UserInterfaceTheme.h
@@ -0,0 +1,42 @@
+/** @file
+  Copyright (C) 2005 - 2017, Apple Inc.  All rights reserved.
+  Portions Copyright (C) 2017, CupertinoNet.  All rights reserved.<BR>
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+**/
+
+#ifndef USER_INTERFACE_THEME_H_
+#define USER_INTERFACE_THEME_H_
+
+// EFI_USER_INTERFACE_THEME_PROTOCOL_GUID
+#define EFI_USER_INTERFACE_THEME_PROTOCOL_GUID  \
+  { 0xD5B0AC65, 0x9A2D, 0x4D2A,                 \
+    { 0xBB, 0xD6, 0xE8, 0x71, 0xA9, 0x5E, 0x04, 0x35 } }
+
+// UI_THEME_GET_BACKGROUND_COLOR
+typedef
+EFI_STATUS
+(EFIAPI *UI_THEME_GET_BACKGROUND_COLOR)(
+  IN OUT UINT32  *BackgroundColor
+  );
+
+// EFI_USER_INTERFACE_THEME_PROTOCOL
+typedef struct {
+  UINTN                         Revision;            ///< Revision.
+  UI_THEME_GET_BACKGROUND_COLOR GetBackgroundColor;  ///< Present as of Revision 1.
+} EFI_USER_INTERFACE_THEME_PROTOCOL;
+
+// gEfiUserInterfaceThemeProtocolGuid
+extern EFI_GUID gEfiUserInterfaceThemeProtocolGuid;
+
+#endif // USER_INTERFACE_THEME_H_
