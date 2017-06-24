4d48ca939475143e1aae13f7eedcf515a78fbb7f
Download-Fritz <download-fritz@outlook.com>
2017-06-24 19:47:11 +0200

    Add Product Info PlatoformInfo GUID.

--- Include/Guid/ApplePlatformInfo.h
+++ Include/Guid/ApplePlatformInfo.h
@@ -22,15 +22,33 @@
   { 0xD1A04D55, 0x75B9, 0x41A3,                 \
     { 0x90, 0x36, 0x8F, 0x4A, 0x26, 0x1C, 0xBB, 0xA2 } }
 
-// APPLE_PLATFORM_INFO_KEYBOARD_GUID
-#define APPLE_PLATFORM_INFO_KEYBOARD_GUID                 \
-  { 0x51871CB9, 0xE25D, 0x44B4,                           \
-    { 0x96, 0x99, 0x0E, 0xE8, 0x64, 0x4C, 0xED, 0x69 } }
-
 // gAppleFsbFrequencyPlatformInfoGuid
 extern EFI_GUID gAppleFsbFrequencyPlatformInfoGuid;
 
+// APPLE_PLATFORM_INFO_KEYBOARD_GUID
+#define APPLE_PLATFORM_INFO_KEYBOARD_GUID  \
+  { 0x51871CB9, 0xE25D, 0x44B4,            \
+    { 0x96, 0x99, 0x0E, 0xE8, 0x64, 0x4C, 0xED, 0x69 } }
+
+// APPLE_KEYBOARD_INFO
+typedef UINT32 APPLE_KEYBOARD_INFO;
+
 // gAppleKeyboardPlatformInfoGuid
 extern EFI_GUID gAppleKeyboardPlatformInfoGuid;
+
+// APPLE_PRODUCT_INFO_PLATFORM_INFO_GUID
+#define APPLE_PRODUCT_INFO_PLATFORM_INFO_GUID  \
+  { 0xBF7F6F3A, 0x5523, 0x488E,                \
+    { 0x8A, 0x60, 0xF0, 0x48, 0x63, 0xB9, 0x75, 0xC3 } }
+
+// APPLE_PRODUCT_INFO
+typedef struct {
+  UINT8  Unknwon[86];
+  CHAR8  Model[64];
+  CHAR16 Family[64];
+} APPLE_PRODUCT_INFO;
+
+// gAppleProductInfoPlatformInfoGuid
+extern EFI_GUID gAppleProductInfoPlatformInfoGuid;
 
 #endif // APPLE_PLATFORM_INFO_H_
