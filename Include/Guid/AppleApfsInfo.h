481cdd8f8743a00a72259dd07eca2bee6d5cc457
Download-Fritz <download-fritz@outlook.com>
2017-12-10 01:49:39 +0100

    Import AppleApfsInfo.h.

---
+++ Include/Guid/AppleApfsInfo.h
@@ -0,0 +1,50 @@
+/** @file
+  Copyright (c) 2005 - 2017, Apple Inc.  All rights reserved.
+  Portions Copyright (C) 2017, CupertinoNet.  All rights reserved.<BR>
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+**/
+
+#ifndef APPLE_APFS_INFO_H_
+#define APPLE_APFS_INFO_H_
+
+#define APPLE_APFS_CONTAINER_INFO_GUID  \
+  { 0x3533CF0D, 0x685F, 0x5EBF,         \
+    { 0x8D, 0xC6, 0x73, 0x93, 0x48, 0x5B, 0xAF, 0xA2 } }
+
+typedef struct {
+  UINT32 Always1;
+  GUID   Uuid;
+} APPLE_APFS_CONTAINER_INFO;
+
+extern EFI_GUID gAppleApfsContainerInfoGuid;
+
+#define APPLE_APFS_VOLUME_INFO_GUID  \
+  { 0x900C7693, 0x8C14, 0x58BA,      \
+    { 0xB4, 0x4E, 0x97, 0x45, 0x15, 0xD2, 0x7C, 0x78 } }
+
+
+#define APPLE_APFS_VOLUME_ROLE_RECOVERY  BIT2
+#define APPLE_APFS_VOLUME_ROLE_PREBOOT   BIT4
+
+typedef UINT32 APPLE_APFS_VOLUME_ROLE;
+
+typedef struct {
+  UINT32                 Always1;
+  GUID                   Uuid;
+  APPLE_APFS_VOLUME_ROLE Role;
+} APPLE_APFS_VOLUME_INFO;
+
+extern EFI_GUID gAppleApfsVolumeInfoGuid;
+
+#endif // APPLE_APFS_H_
