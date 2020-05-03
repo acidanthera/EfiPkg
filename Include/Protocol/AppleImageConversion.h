254b0821ce0d48a1221009832b1e660126be0842
vit9696 <vit9696@users.noreply.github.com>
2020-05-04 01:14:35 +0300

    AppleImageConversion: Provide full documentation

--- Include/Protocol/AppleImageConversion.h
+++ Include/Protocol/AppleImageConversion.h
@@ -25,6 +25,8 @@ WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 
 //
 // Protocol revision
+// Starting with this version scaled interfaces wered added.
+// Older versions had none.
 //
 #define APPLE_IMAGE_CONVERSION_PROTOCOL_REVISION 0x20000
 
@@ -34,6 +36,17 @@ WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 //
 #define APPLE_IMAGE_CONVERSION_PROTOCOL_ANY_EXTENSION 0
 
+/**
+  Recognise image passed through the buffer.
+
+  @param[in]  ImageBuffer   Buffer containing image data.
+  @param[in]  ImageSize     Size of the buffer.
+
+  @retval EFI_INVALID_PARAMETER when ImageBuffer is NULL.
+  @retval EFI_INVALID_PARAMETER when ImageSize is 0.
+  @retval EFI_UNSUPPORTED       when image is not supported (e.g. too large or corrupted).
+  @retval EFI_SUCCESS           when image can be decoded.
+**/
 typedef
 EFI_STATUS
 (EFIAPI* RECOGNIZE_IMAGE_DATA) (
@@ -41,6 +54,21 @@ EFI_STATUS
   IN UINTN                ImageSize
   );
 
+/**
+  Get image dimensions.
+
+  @param[in]  ImageBuffer   Buffer containing image data.
+  @param[in]  ImageSize     Size of the buffer.
+  @param[out] ImageWidth    Image width in pixels.
+  @param[out] ImageHeight   Image height in pixels.
+
+  @retval EFI_INVALID_PARAMETER when ImageBuffer is NULL.
+  @retval EFI_INVALID_PARAMETER when ImageSize is 0.
+  @retval EFI_INVALID_PARAMETER when ImageWidth is NULL.
+  @retval EFI_INVALID_PARAMETER when ImageHeight is NULL.
+  @retval EFI_UNSUPPORTED       when image is not supported (e.g. too large or corrupted).
+  @retval EFI_SUCCESS           when image dimensions were read.
+**/
 typedef
 EFI_STATUS
 (EFIAPI* GET_IMAGE_DIMS) (
@@ -50,15 +78,56 @@ EFI_STATUS
   OUT UINT32              *ImageHeight
   );
 
+/**
+  Decode image data in 32-bit format.
+
+  @param[in]     ImageBuffer       Buffer containing image data.
+  @param[in]     ImageSize         Size of the buffer.
+  @param[in,out] RawImageData      Pointer to decoded buffer pointer.
+                                   - When NULL it is allocated from pool.
+                                   - When not NULL provided buffer is used.
+  @param[in,out] RawImageDataSize  Decoded buffer size.
+                                   - Set to allocated area size if allocated.
+                                   - Set to truncated area size when provided buffer is used.
+                                   - Set to required area size when provided buffer is too small.
+
+  @retval EFI_INVALID_PARAMETER when ImageBuffer is NULL.
+  @retval EFI_INVALID_PARAMETER when ImageSize is 0.
+  @retval EFI_INVALID_PARAMETER when RawImageData is NULL.
+  @retval EFI_INVALID_PARAMETER when RawImageDataSize is NULL.
+  @retval EFI_UNSUPPORTED       when image is not supported (e.g. too large or corrupted).
+  @retval EFI_OUT_OF_RESOURCES  when allocation error happened.
+  @retval EFI_BUFFER_TOO_SMALL  when provided buffer is too small, RawImageDataSize is updated.
+  @retval EFI_SUCCESS           when image was decoded successfully.
+**/
 typedef
 EFI_STATUS
 (EFIAPI* DECODE_IMAGE_DATA) (
   IN     VOID            *ImageBuffer,
   IN     UINTN           ImageSize,
   IN OUT EFI_UGA_PIXEL   **RawImageData,
-     OUT UINTN           *RawImageDataSize
+  IN OUT UINTN           *RawImageDataSize
   );
 
+/**
+  Get image dimensions for scale.
+  Protocol revision APPLE_IMAGE_CONVERSION_PROTOCOL_REVISION or higher is required.
+
+  @param[in]  ImageBuffer   Buffer containing image data.
+  @param[in]  ImageSize     Size of the buffer.
+  @param[in]  Scale         Scaling factor (e.g. 1 or 2).
+  @param[out] ImageWidth    Image width in pixels.
+  @param[out] ImageHeight   Image height in pixels.
+
+  @retval EFI_INVALID_PARAMETER when ImageBuffer is NULL.
+  @retval EFI_INVALID_PARAMETER when ImageSize is 0.
+  @retval EFI_INVALID_PARAMETER when Scale is 0.
+  @retval EFI_INVALID_PARAMETER when ImageWidth is NULL.
+  @retval EFI_INVALID_PARAMETER when ImageHeight is NULL.
+  @retval EFI_UNSUPPORTED       when Scale is not supported.
+  @retval EFI_UNSUPPORTED       when image is not supported (e.g. too large or corrupted).
+  @retval EFI_SUCCESS           when image dimensions were read.
+**/
 typedef
 EFI_STATUS
 (EFIAPI* GET_IMAGE_DIMS_EX) (
@@ -69,6 +138,31 @@ EFI_STATUS
   OUT UINT32             *ImageHeight
   );
 
+/**
+  Decode image data in 32-bit format.
+  Protocol revision APPLE_IMAGE_CONVERSION_PROTOCOL_REVISION or higher is required.
+
+  @param[in]     ImageBuffer       Buffer containing image data.
+  @param[in]     ImageSize         Size of the buffer.
+  @param[in]     Scale             Scaling factor (e.g. 1 or 2).
+  @param[in,out] RawImageData      Pointer to decoded buffer pointer.
+                                   - When NULL it is allocated from pool.
+                                   - When not NULL provided buffer is used.
+  @param[in,out] RawImageDataSize  Decoded buffer size.
+                                   - Set to allocated area size if allocated.
+                                   - Set to truncated area size when provided buffer is used.
+                                   - Set to required area size when provided buffer is too small.
+
+  @retval EFI_INVALID_PARAMETER when ImageBuffer is NULL.
+  @retval EFI_INVALID_PARAMETER when ImageSize is 0.
+  @retval EFI_INVALID_PARAMETER when RawImageData is NULL.
+  @retval EFI_INVALID_PARAMETER when RawImageDataSize is NULL.
+  @retval EFI_UNSUPPORTED       when Scale is not supported.
+  @retval EFI_UNSUPPORTED       when image is not supported (e.g. too large or corrupted).
+  @retval EFI_OUT_OF_RESOURCES  when allocation error happened.
+  @retval EFI_BUFFER_TOO_SMALL  when provided buffer is too small, RawImageDataSize is updated.
+  @retval EFI_SUCCESS           when image was decoded successfully.
+**/
 typedef
 EFI_STATUS
 (EFIAPI* DECODE_IMAGE_DATA_EX) (
@@ -76,9 +170,12 @@ EFI_STATUS
   IN     UINTN           ImageSize,
   IN     UINTN           Scale,
   IN OUT EFI_UGA_PIXEL   **RawImageData,
-     OUT UINTN           *RawImageDataSize
+  IN OUT UINTN           *RawImageDataSize
   );
 
+/**
+  Apple image conversion protocol definition.
+**/
 typedef struct APPLE_IMAGE_CONVERSION_PROTOCOL_ {
   UINT64                 Revision;
   UINTN                  FileExt;
