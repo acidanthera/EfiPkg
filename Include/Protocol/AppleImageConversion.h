c0bcb4e0270403ae31837df679c92598b900eff5
vit9696 <vit9696@users.noreply.github.com>
2020-05-03 23:48:36 +0300

    AppleImageConversion: Fix scale argument

--- Include/Protocol/AppleImageConversion.h
+++ Include/Protocol/AppleImageConversion.h
@@ -3,6 +3,7 @@ Copyright (C) 2012, tiamo.  All rights reserved.<BR>
 Copyright (C) 2014, dmazar.  All rights reserved.<BR>
 Copyright (C) 2018, savvas.  All rights reserved.<BR>
 Copyright (C) 2018, Download-Fritz.  All rights reserved.<BR>
+Copyright (C) 2019-2020, vit9696.  All rights reserved.<BR>
 This program and the accompanying materials are licensed and made available
 under the terms and conditions of the BSD License which accompanies this
 distribution.  The full text of the license may be found at
@@ -15,6 +16,8 @@ WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 
 #ifndef APPLE_IMAGE_CONVERSION_H
 #define APPLE_IMAGE_CONVERSION_H
+
+#include <Protocol/UgaDraw.h>
 
 #define APPLE_IMAGE_CONVERSION_PROTOCOL_GUID          \
   { 0x0DFCE9F6, 0xC4E3, 0x45EE,                       \
@@ -30,11 +33,6 @@ WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 // possibly by chainloading other files
 //
 #define APPLE_IMAGE_CONVERSION_PROTOCOL_ANY_EXTENSION 0
-
-//
-// Interface version for Ver-suffixed functions
-//
-#define APPLE_IMAGE_CONVERSION_PROTOCOL_INTERFACE_V1 1
 
 typedef
 EFI_STATUS
@@ -63,33 +61,32 @@ EFI_STATUS
 
 typedef
 EFI_STATUS
-(EFIAPI* GET_IMAGE_DIMS_VER) (
+(EFIAPI* GET_IMAGE_DIMS_EX) (
   IN  VOID               *ImageBuffer,
   IN  UINTN              ImageSize,
-  IN  UINTN              Version,
+  IN  UINTN              Scale,
   OUT UINT32             *ImageWidth,
   OUT UINT32             *ImageHeight
   );
 
 typedef
 EFI_STATUS
-(EFIAPI* DECODE_IMAGE_DATA_VER) (
+(EFIAPI* DECODE_IMAGE_DATA_EX) (
   IN     VOID            *ImageBuffer,
   IN     UINTN           ImageSize,
-  IN     UINTN           Version,
+  IN     UINTN           Scale,
   IN OUT EFI_UGA_PIXEL   **RawImageData,
      OUT UINTN           *RawImageDataSize
   );
 
-typedef struct APPLE_IMAGE_CONVERSION_PROTOCOL_
-{
+typedef struct APPLE_IMAGE_CONVERSION_PROTOCOL_ {
   UINT64                 Revision;
   UINTN                  FileExt;
   RECOGNIZE_IMAGE_DATA   RecognizeImageData;
   GET_IMAGE_DIMS         GetImageDims;
   DECODE_IMAGE_DATA      DecodeImageData;
-  GET_IMAGE_DIMS_VER     GetImageDimsVer;
-  DECODE_IMAGE_DATA_VER  DecodeImageDataVer;
+  GET_IMAGE_DIMS_EX      GetImageDimsEx;
+  DECODE_IMAGE_DATA_EX   DecodeImageDataEx;
 } APPLE_IMAGE_CONVERSION_PROTOCOL;
 
 extern EFI_GUID gAppleImageConversionProtocolGuid;
